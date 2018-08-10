589643e6d72b890631eec0afa0f7550e1452d864
NOORABDELHALIM <nur.abdelhalim@rebels.ridgenet.org>
2018-08-10 15:49:13 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -6,40 +6,42 @@ class Book
   def initialize(title)
     @title = title 
   end 
- def title
-    @title 
-  end 
+  attr_accessor:title, :author, :page_count, :turn_page
+# def title
+#     @title 
+#   end 
   
   
   
-  def author=(author)
-    @author = author
-  end 
-  def author
-    @author 
-  end 
+  # def author=(author)
+  #   @author = author
+  # end 
+  # def author
+  #   @author 
+  # end 
   
   
   
-  def page_count=(page_count)
-    @page_count = page_count
-  end 
-  def page_count
-    @page_count
-  end
+  # def page_count=(page_count)
+  #   @page_count = page_count
+  # end 
+  # def page_count
+  #   @page_count
+  # end
   
   
   
-  def genre=(genre)
-    @genre = genre 
-  end 
-  def genre 
-    @genre 
-  end 
+  # def genre=(genre)
+  #   @genre = genre 
+  # end 
+  # def genre 
+  #   @genre 
+  # end 
   
   
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end 
 end 
+
 
